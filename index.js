141b16bd6faef4504000444cff009e371dc40fa7
Mauricio <cm.mohedano@gmail.com>
2019-06-25 21:02:02 -0500

    Add mongo-connection
    * Post
    * Delete
    * Update
    * Get

--- index.js
+++ index.js
@@ -16,10 +16,65 @@ app.use(bodyParser.json())
 
 
 app.get('/demo', (req, res)=>{
-console.log("demo funcionando");
-res.send("demo funcionando")
-
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, db) =>{
+        if (err) throw err;
+        var dbo = db.db("xuxo");
+        dbo.collection("Users").find({}).toArray(function(err, result) {
+          if (err) throw err;
+           res.send(result)
+          db.close();
+        });
+      });
 })
+
+app.post('/postDemo', (req, res) => {
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("xuxo");
+        var myobj = { 
+                        username: req.body.username,
+                        password: req.body.password 
+                    };
+        dbo.collection("Users").insertOne(myobj, (err, res) => {
+          if (err) throw err;
+          console.log("1 document inserted");
+         
+          db.close();
+        });
+      });
+      res.send({msg:"All ok"})
+})
+
+app.put('/updateDemo', (req,res)=>{
+    MongoClient.connect(url, { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("xuxo");
+        var myquery = { _id : ObjectId(req.body.id)};
+        var newvalues = { $set: {username: req.body.username, password: req.body.password } };
+        dbo.collection("Users").updateOne(myquery, newvalues, function(err, res) {
+          if (err) throw err;
+          console.log("1 document updated");
+          db.close();
+        });
+      });
+    res.send({msg:"ALL OK"})
+})  
+
+app.delete('/deleteDemo' , (req,res)=>{
+
+    MongoClient.connect(url, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("xuxo");
+        var myquery = { _id : ObjectId(req.body.id) };
+        dbo.collection("Users").deleteOne(myquery, function(err, obj) {
+          if (err) throw err;
+          console.log("1 document deleted");
+          db.close();
+        });
+      });
+    res.send({msg:"ALL_OK"})
+})
+
 
 app.listen(port, ()=>{
     console.log("Server online");
