e3f2fad4d877a55f8971193198b70f16990637bf
Juan <jcontmar@gmail.com>
2019-06-25 20:31:06 -0500

    Base-back-end
    * express
    * cors
    * mongo connection string
    * body-parser

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const express = require('express');
-var cors = require('cors');
-const app = express();
-app.use(cors());
-const os = require('os')
-let bodyParser = require('body-parser')
-
-var MongoClient = require('mongodb').MongoClient;
-let url = "mongodb+srv://admin:admin@cluster0-h9sgx.mongodb.net/test?retryWrites=true";
+const express = require('express')
+const app = express()
+let bodyParser = require('body-parser');
+const port = 3001
+let MongoClient = require('mongodb').MongoClient;
 let ObjectId = require('mongodb').ObjectId;
+let url = "mongodb+srv://admin:admin@cluster0-ytsob.mongodb.net/test?retryWrites=true&w=majority"
 
-const port = 3001;
+var cors = require('cors')
 
+app.use(cors());
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("claseJs");
-        dbo.collection("users").find({}).toArray( function(err, result) {
-          if (err) throw err;
-          
-          res.send(result)
-          db.close();
-        });
-      });
-});
-app.listen(port, function () {
-    console.log('Example app listening on port 3000!');
-  });
-  
+app.get('/demo', (req, res)=>{
+console.log("demo funcionando");
+res.send("demo funcionando")
+
+})
+
+app.listen(port, ()=>{
+    console.log("Server online");
+    
+})
