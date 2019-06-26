4137469be13f5dc59755577df732ce785d7e237b
Mauricio <cm.mohedano@gmail.com>
2019-06-25 21:24:17 -0500

    Create sample microservices

---
+++ routes/index.js
@@ -0,0 +1,65 @@
+let MongoClient = require('mongodb').MongoClient;
+let ObjectId = require('mongodb').ObjectId;
+let url = "mongodb+srv://admin:admin@cluster0-ytsob.mongodb.net/test?retryWrites=true&w=majority"
+module.exports = (app) => {
+    app.get('/demo', (req, res)=>{
+        MongoClient.connect(url, { useNewUrlParser: true }, (err, db) =>{
+            if (err) throw err;
+            var dbo = db.db("xuxo");
+            dbo.collection("Users").find({}).toArray(function(err, result) {
+              if (err) throw err;
+               res.send(result)
+              db.close();
+            });
+          });
+    })
+    
+    app.post('/postDemo', (req, res) => {
+        MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+            if (err) throw err;
+            var dbo = db.db("xuxo");
+            var myobj = { 
+                            username: req.body.username,
+                            password: req.body.password 
+                        };
+            dbo.collection("Users").insertOne(myobj, (err, res) => {
+              if (err) throw err;
+              console.log("1 document inserted");
+             
+              db.close();
+            });
+          });
+          res.send({msg:"All ok"})
+    })
+    
+    app.put('/updateDemo', (req,res)=>{
+        MongoClient.connect(url, { useNewUrlParser: true } , function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("xuxo");
+            var myquery = { _id : ObjectId(req.body.id)};
+            var newvalues = { $set: {username: req.body.username, password: req.body.password } };
+            dbo.collection("Users").updateOne(myquery, newvalues, function(err, res) {
+              if (err) throw err;
+              console.log("1 document updated");
+              db.close();
+            });
+          });
+        res.send({msg:"ALL OK"})
+    })  
+    
+    app.delete('/deleteDemo' , (req,res)=>{
+    
+        MongoClient.connect(url, (err, db) => {
+            if (err) throw err;
+            var dbo = db.db("xuxo");
+            var myquery = { _id : ObjectId(req.body.id) };
+            dbo.collection("Users").deleteOne(myquery, function(err, obj) {
+              if (err) throw err;
+              console.log("1 document deleted");
+              db.close();
+            });
+          });
+        res.send({msg:"ALL_OK"})
+    })
+}
+
